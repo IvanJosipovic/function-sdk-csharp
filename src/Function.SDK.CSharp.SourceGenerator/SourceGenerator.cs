25bacb560c54e53dd5da4a08310ece02211ca257
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-03-02 10:24:08 -0800

    clean

--- src/Function.SDK.CSharp.SourceGenerator/SourceGenerator.cs
+++ src/Function.SDK.CSharp.SourceGenerator/SourceGenerator.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.Json.Nodes;
-using KubernetesCRDModelGen;
 using KubernetesCRDModelGen.Base;
 using Microsoft.CodeAnalysis;
 using Microsoft.OpenApi;
