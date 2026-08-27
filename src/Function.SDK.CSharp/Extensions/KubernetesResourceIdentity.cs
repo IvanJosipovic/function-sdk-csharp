2aa4306bf9af9456fa8d3c94e743ac7d821aed57
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2026-08-27 08:29:45 -0700

    feat: add extensions

---
+++ src/Function.SDK.CSharp/Extensions/KubernetesResourceIdentity.cs
@@ -0,0 +1,33 @@
+using k8s;
+
+namespace Function.SDK.CSharp;
+
+internal static class KubernetesResourceIdentity
+{
+    internal static (string ApiVersion, string Kind) Get<T>()
+    {
+        var metadata = typeof(T).GetKubernetesTypeMetadata();
+        var apiVersion = string.IsNullOrEmpty(metadata.Group)
+            ? metadata.ApiVersion
+            : $"{metadata.Group}/{metadata.ApiVersion}";
+
+        return (apiVersion, metadata.Kind);
+    }
+
+    internal static string CreateKey<T>(string key)
+    {
+        var identity = Get<T>();
+
+        return CreateKey(identity.ApiVersion, identity.Kind, key);
+    }
+
+    internal static string CreateKey(IKubernetesObject resource, string key)
+    {
+        return CreateKey(resource.ApiVersion, resource.Kind, key);
+    }
+
+    private static string CreateKey(string apiVersion, string kind, string key)
+    {
+        return $"{apiVersion}/{kind}/{key}";
+    }
+}
